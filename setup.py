28fba7c25802b23df09468a19e7c62e1a0158f37
nullpunktTUD <sipreuss@gmail.com>
2021-10-22 16:32:27 +0200

    Added dependencies

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import setup
 import glob
 
 setup(name='tud-ais',
-      version='0.1.5',
+      version='0.1.6',
       description='Supplemental material for the lecture Angewandte Intelligente Signalverarbeitung at TU Dresden',
       url='http://github.com/TUD-STKS/tud-ais-package',
       author='Simon Stone',
       author_email='simon.stone@tu-dresden.de',
       license='MIT',
       packages=['tud-ais'],
-      include_package_data=True
+      include_package_data=True,
+      install_requires=[
+          'jupyterlab',
+          'pandas',
+          'scikit-learn',
+      ],
       )
