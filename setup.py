3dea8fa62d053601f2a2543307cde2023de3d161
nullpunktTUD <sipreuss@gmail.com>
2021-10-22 12:55:51 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tud-ais',
+      version='0.1',
+      description='Supplemental material for the lecture Angewandte Intelligente Signalverarbeitung at TU Dresden',
+      url='http://github.com/TUD-STKS/tud-ais-package',
+      author='Simon Stone',
+      author_email='simon.stone@tu-dresden.de',
+      license='MIT',
+      packages=['tud-ais']
+      )
