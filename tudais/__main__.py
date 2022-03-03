a0693102574c304c27f768de9a7241edebca41a8
Simon Stone <sipreuss@gmail.com>
2022-03-03 09:55:45 +0100

    adds a getter for the notebook directory

--- tudais/__main__.py
+++ tudais/__main__.py
@@ -3,9 +3,20 @@ from pathlib import Path
 import tudthemes
 
 
-def start_jupyter_server():
+def get_notebook_dir():
+    """
+    Returns a string containing the path to the notebook directory
+    """
     pkg_root = Path(os.path.dirname(__file__))
-    notebook_dir = str(pkg_root / Path('notebooks'))
+    return str(pkg_root / Path('notebooks'))
+
+
+def start_jupyter_server():
+    """
+    Start a Jupyter server with notebook theming
+    """
+    pkg_root = Path(os.path.dirname(__file__))
+    notebook_dir = get_notebook_dir()
     tudthemes.start_notebook(theme_select='bright', notebook_dir=notebook_dir)
 
 
